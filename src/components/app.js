245c0987efc995973fe35ac7d6ae417d1245338d
Paulo <prc.rgc@gmail.com>
2022-10-28 09:48:25 -0300

    .

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import Contador from './components/contador'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App(){
+    return(
+    <div>
+        <Contador/>
+    </div>
+  );
+}
+
+export default App;
